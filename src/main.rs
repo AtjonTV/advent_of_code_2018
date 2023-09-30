b249a72a99ca06ff9d53a01ce08ac8a732e059cb
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2023-09-30 20:49:08 +0200

    Implement Day 2 Part 2
    I here again was able to improve performance going from 2 seconds to 30ms.
    The solution was to use ids.sort() and a sliding window.
    Before that I iterated over ids twice, making 28861 iterations before a match.

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,14 @@ use std::fmt::Debug;
 use std::fs;
 use std::ops::Sub;
 use std::time::Instant;
+use levenshtein::levenshtein;
 
 #[allow(dead_code)]
 enum Solutions {
     Day1Part1,
     Day1Part2,
     Day2Part1,
+    Day2Part2,
 }
 
 fn main() {
@@ -23,6 +25,7 @@ fn main() {
     run_solution(Solutions::Day1Part1, EXAMPLE);
     run_solution(Solutions::Day1Part2, EXAMPLE);
     run_solution(Solutions::Day2Part1, EXAMPLE);
+    run_solution(Solutions::Day2Part2, EXAMPLE);
     println!("Finished in {:?}", Instant::now().sub(app_start));
 }
 
@@ -31,6 +34,7 @@ fn run_solution(solution: Solutions, use_example: bool) {
         Solutions::Day1Part1 => day1_part1(use_example),
         Solutions::Day1Part2 => day1_part2(use_example),
         Solutions::Day2Part1 => day2_part1(use_example),
+        Solutions::Day2Part2 => day2_part2(use_example),
     }
 }
 
@@ -54,7 +58,7 @@ fn read_file_by_lines(path: &str) -> Vec<String> {
 
 fn assert<T: PartialEq + Debug>(use_example: bool, expect_example: T, expect: T, actual: T) {
     if use_example {
-        assert_eq!(expect_example, expect);
+        assert_eq!(expect_example, actual);
     } else {
         assert_eq!(expect, actual);
     }
@@ -143,6 +147,77 @@ fn day2_part1(use_example: bool) {
     println!("Day 2 Part 1 ({:?}): {}", Instant::now().sub(solution_start), result);
 }
 
+fn day2_part2(use_example: bool) {
+    let solution_start = Instant::now();
+    let input_path = get_input_path("day2", "part2", use_example);
+    let input = read_file_by_lines(&input_path);
+
+    let mut ids: Vec<String> = Vec::new();
+    for line in input {
+        let line_result =
+            line
+                .chars()
+                .fold(HashMap::new(), |mut map, c| {
+                    if map.contains_key(&c) {
+                        if let Some(count) = map.get(&c) {
+                            map.insert(c, count + 1);
+                        }
+                    } else {
+                        map.insert(c, 1);
+                    }
+                    map
+                });
+        let has_two_match = line_result
+            .iter()
+            .find_map(|(&key, &val)| if val == 2 { Some(key) } else { None })
+            .iter()
+            .count();
+        let has_three_match = line_result
+            .iter()
+            .find_map(|(&key, &val)| if val == 3 { Some(key) } else { None })
+            .iter()
+            .count();
+
+        if has_two_match != 0  || has_three_match != 0 {
+            ids.push((*line.trim()).to_string());
+        }
+    }
+
+    // sort the ids so that we can use a sliding window to find the ids we need
+    ids.sort();
+
+    let mut ids_match: Option<(String, String)> = None;
+    let mut ids_window = ids.windows(2);
+    'outer: while let Some(window) = ids_window.next() {
+        if levenshtein(window[0].as_str(), window[1].as_str()) == 1 {
+            ids_match = Some((window[0].clone(), window[1].clone()));
+            break 'outer;
+        }
+    }
+
+    if ids_match.is_none() {
+        println!("Day 2 Part 2: No match found, exiting");
+        return;
+    }
+
+    let (first, second): (String, String) = ids_match.unwrap();
+
+    let mut common_chars: Vec<String> = Vec::new();
+    for i in 0..first.len() {
+        let fchar = first.get(i..(i+1));
+        let schar = second.get(i..(i+1));
+
+        if fchar.is_some() && schar.is_some() {
+            if fchar.unwrap().eq(schar.unwrap()) {
+                common_chars.push(first[i..(i+1)].to_string());
+            }
+        }
+    }
+    let result = common_chars.join("");
+
+    assert(use_example, String::from("1"), String::from("lsrivmotzbdxpkxnaqmuwcchj"), result.clone());
+    println!("Day 2 Part 2 ({:?}): {}", Instant::now().sub(solution_start), result);
+}
 
 #[allow(dead_code)]
 fn example_part(use_example: bool) {
